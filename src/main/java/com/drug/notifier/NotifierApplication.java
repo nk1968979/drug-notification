7d55af840650fa6263668e4f23c87addfff708de
kchaitanya55 <pkrishnachaitanya55@gmail.com>
2022-09-03 11:37:57 +0530

    Adding Prescriptions screen

--- src/main/java/com/drug/notifier/NotifierApplication.java
+++ src/main/java/com/drug/notifier/NotifierApplication.java
@@ -2,15 +2,9 @@ package com.drug.notifier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NotifierApplication {
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(NotifierApplication.class, args);
 	}
